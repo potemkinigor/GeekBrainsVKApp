b09cc41389950cfe5b0be832b80ae4f319f0d76b
Igor Potemkin <potemkinigor@yandex.ru>
2021-01-23 12:20:41 +0300

    Initial commit

---
+++ VKApp/VKApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  VKApp
+//
+//  Created by Igor Potemkin on 23.01.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
