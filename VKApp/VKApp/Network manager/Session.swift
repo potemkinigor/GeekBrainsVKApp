c6a26aeeef7c511e5966e0da55c662ebfaf6be68
Igor Potemkin <potemkinigor@yandex.ru>
2021-03-21 08:53:04 +0300

    ClientServerFirstLesson

---
+++ VKApp/VKApp/Network manager/Session.swift
@@ -0,0 +1,23 @@
+//
+//  Session.swift
+//  VKApp
+//
+//  Created by User on 21.03.2021.
+//
+
+import Foundation
+
+class Session {
+    
+    static var session = Session()
+    
+    var token: String = ""
+    var userid: Int = 1111111111111111111
+    
+    
+    private init () {
+        
+    }
+    
+    
+}
